c324a3f8af8924c2d752d52f24965f8b21037ff7
jpalmat <jimmy.palma1990@gmail.com>
2021-07-09 11:42:54 -0400

    lab3 asd

---
+++ ASD/LABS/lab3/counter/src/main/java/counter/JFrameCounter.java
@@ -0,0 +1,135 @@
+package counter;
+import java.awt.Dimension;
+import java.awt.Toolkit;
+
+import java.awt.Rectangle;
+
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+
+import javax.swing.JButton;
+import javax.swing.JFrame;
+
+//this class is just for communiacting with client
+//we remove donotify from here
+public class JFrameCounter extends JFrame{
+    private JButton jButtonIncrement = new JButton();
+    private JButton jButtondecrement = new JButton();
+    private JButton jButtonundo = new JButton();
+    private JButton jButtonredo = new JButton();
+    private HistoryCommand historyCommand= new HistoryCommand();
+    private Counter counter;
+
+    //in constructor we create counter and frames
+    //Also call add observer here
+    public JFrameCounter() {
+        try {
+            jbInit();
+            counter = new Counter();
+            //Codes for StatePattern
+            CounterState singleDigit = new SingleDigit(counter);
+            counter.setCounterState(singleDigit);
+
+            TextFrame textframe = new TextFrame();
+            textframe.setVisible(true);
+            RectFrame rectframe = new RectFrame();
+            rectframe.setVisible(true);
+            OvalFrame ovalframe = new OvalFrame();
+            ovalframe.setVisible(true);
+            counter.addObserver(textframe);
+            counter.addObserver(rectframe);
+            counter.addObserver(ovalframe);
+
+
+            CountHandler orangeHandler= new OrangeHandler(null);
+            CountHandler blueHandler= new BlueHandler(orangeHandler);
+            CountHandler greenHandler= new GreenHandler(blueHandler);
+            CountHandler redHandler= new RedHandler(greenHandler);
+
+            counter.setCountHandler(redHandler);
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+
+    public static void main(String[] args) {
+        JFrameCounter frame = new JFrameCounter();
+        Dimension screenSize = Toolkit.getDefaultToolkit().getScreenSize();
+        Dimension frameSize = frame.getSize();
+        if (frameSize.height > screenSize.height) {
+            frameSize.height = screenSize.height;
+        }
+        if (frameSize.width > screenSize.width) {
+            frameSize.width = screenSize.width;
+        }
+        frame.setLocation( ( screenSize.width - frameSize.width ) / 2, ( screenSize.height - frameSize.height ) / 2 );
+        frame.setDefaultCloseOperation( JFrame.EXIT_ON_CLOSE );
+        frame.setVisible(true);
+
+    }
+
+    private void jbInit() throws Exception {
+        this.getContentPane().setLayout( null );
+        this.setSize(new Dimension(297, 169));
+        jButtonIncrement.setText("+");
+        jButtonIncrement.setBounds(new Rectangle(30, 25, 73, 22));
+        jButtonIncrement.setActionCommand("increment");
+        jButtonIncrement.addActionListener(new ActionListener() {
+                    public void actionPerformed(ActionEvent e) {
+                        jButtonIncrement_actionPerformed(e);
+                    }
+                });
+        jButtondecrement.setText("-");
+        jButtondecrement.setBounds(new Rectangle(155, 25, 73, 22));
+        jButtondecrement.setActionCommand("decrement");
+        jButtondecrement.addActionListener(new ActionListener() {
+                    public void actionPerformed(ActionEvent e) {
+                        jButtondecrement_actionPerformed(e);
+                    }
+                });
+        jButtonundo.setText("undo");
+        jButtonundo.setBounds(new Rectangle(30, 80, 73, 22));
+        jButtonundo.addActionListener(new ActionListener() {
+                    public void actionPerformed(ActionEvent e) {
+                        jButtonundo_actionPerformed(e);
+                    }
+                });
+        jButtonredo.setText("redo");
+        jButtonredo.setBounds(new Rectangle(155, 80, 73, 22));
+        jButtonredo.addActionListener(new ActionListener() {
+                    public void actionPerformed(ActionEvent e) {
+                        jButtonredo_actionPerformed(e);
+                    }
+                });
+        this.getContentPane().add(jButtonredo, null);
+        this.getContentPane().add(jButtonundo, null);
+        this.getContentPane().add(jButtondecrement, null);
+        this.getContentPane().add(jButtonIncrement, null);
+    }
+
+    private void jButtonIncrement_actionPerformed(ActionEvent e) {
+        CounterCommands incrementCommand = new IncrementCommand(counter);
+        incrementCommand.execute();
+        historyCommand.addCommand(incrementCommand);
+      //counter.increment();
+    }
+
+    private void jButtondecrement_actionPerformed(ActionEvent e) {
+        CounterCommands decrementCommand = new DecrementCommand(counter);
+        decrementCommand.execute();
+        historyCommand.addCommand(decrementCommand);
+    	//counter.decrement();
+    }
+
+    private void jButtonundo_actionPerformed(ActionEvent e) {
+    	System.out.println("undo");
+    	historyCommand.undo();
+    }
+
+    private void jButtonredo_actionPerformed(ActionEvent e) {
+    	System.out.println("redo");
+    	historyCommand.redo();
+    }
+
+
+}
